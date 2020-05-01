9656179c0eba40e766db91f58e156ad4d6e13800
tayyab203 <61288485+tayyab203@users.noreply.github.com>
2020-05-01 20:48:54 +0500

    commit 6

---
+++ script.js
@@ -0,0 +1,49 @@
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game');
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
+
+// List of words for game
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependent',
+    'steer',
+    'silver',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving',
+    'tayyab',
+    'bajwa',
+    'talha',
+];
+
+// Init word
+let randomWord;
+
+// Init score
+let score = 0;
+
+// Init time
+let time = 10;
+
+// generate random word form array
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
